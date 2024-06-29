31ad008c0bdf87ba0b38c5cc0c951b8068f8935e
Hanzla-Nouman <hanzlanouman514@gmail.com>
2024-06-30 00:19:35 +0500

    Implemented chart

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,11 +1,22 @@
+import DoughnutChart from '@/components/ui/DoughnutChart'
+import HeaderBox from '@/components/ui/HeaderBox'
+import TotalBalance from '@/components/ui/TotalBalance'
 import React from 'react'
 
 const Home = () => {
+    const loggedIn = {firstName: 'John'}
   return (
     <section className='home'>
         <div className="home-content">
             <header className='home-header'>
-                Hi Hanzla,
+               
+                <HeaderBox
+                type="greeting"
+                title="Welcome"
+                user={loggedIn?.firstName || "Guest"}
+                subtext="Access and manage your account and transactions efficiently"
+                />
+                <TotalBalance accounts={[]} totalBanks={1} totalCurrentBalance={1250.35}/>
             </header>
         </div>
         
