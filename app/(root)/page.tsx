862c213381d442dcdb2e80e4240c7c3cfe112d0e
Hanzla-Nouman <hanzlanouman514@gmail.com>
2024-06-29 18:07:34 +0500

    Implement home

---
+++ app/(root)/page.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <section className='home'>
+        <div className="home-content">
+            <header className='home-header'>
+                Hi Hanzla,
+            </header>
+        </div>
+        
+    </section>
+  )
+}
+
+export default Home
